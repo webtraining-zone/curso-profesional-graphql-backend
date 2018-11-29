0f234e4cc4b8cf172fa8e1735717870266310b32
Alex Arriaga <alexzander.arriaga@base22.com>
2018-11-29 11:50:48 -0600

    Agregar ejemplo de código base para Prisma

---
+++ generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/alex-arriaga-5d13f1/curso-profesional-graphql-backend/dev`
+});
+exports.prisma = new exports.Prisma();
