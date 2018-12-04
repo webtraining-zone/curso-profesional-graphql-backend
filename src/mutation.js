6670fb65affd5eaeb4166b50008c300a0e25cdf5
Alex Arriaga <alexzander.arriaga@base22.com>
2018-12-03 19:52:06 -0600

    Agregar ejemplo de mutación que realmente modifica nuestra base de datos remota

--- src/mutation.js
+++ src/mutation.js
@@ -1,8 +1,27 @@
 const Mutation = {
-  createBook(parent, args, context, info) {
+  async createBook(parent, args, context, info) {
     // Create our actual book
-    console.log(args.title);
+    // console.log(args.title);
+
+    // We will have a mutation to be called as this:
+    // mutation {
+    //   createBook(
+    //       title:"JavaScript the Good Parts"
+    // ) {
+    //     title
+    //   }
+    // }
+
+    // @TODO: this operation would only be available for authenticated users
+    const book = await context.db.mutation.createBook({
+      data: {
+        ...args, // title: args.title, etc.
+      },
+    }, info);
+
+    return book;
   },
+
 };
 
 module.exports = Mutation;
--- src/query.js
+++ src/query.js
@@ -1,6 +1,12 @@
 const Query = {
-  books(parent, args, context, info) {
-    return [{title: 'You don\'t know JS'}, {title: 'Eloquent JavaScript'}];
+  // books(parent, args, context, info) {
+  //   return [{title: 'You don\'t know JS'}, {title: 'Eloquent JavaScript'}];
+  // },
+
+  async books(parent, args, context, info) {
+    const books = await context.db.query.books();
+
+    return books;
   },
 };
 
