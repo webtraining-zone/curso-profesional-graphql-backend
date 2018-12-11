daa00e3bd20d39d55bb423b1832dfe33b0918d3e
Alex Arriaga <alexzander.arriaga@base22.com>
2018-12-10 18:52:01 -0600

    Agregar otros dominios a configuración de CORS

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const server = createServer();
 server.start({
   cors: {
     credentials: true,
-    origin: ['http://localhost:8085', 'http://graphql-frontend.webtraining.fun'],
+    origin: ['http://localhost:8085', 'http://graphql-frontend.webtraining.fun', 'http://localhost:4400/', 'http://localhost:3000/'],
     // methods: 'OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE',
     // allowedHeaders: [
     //   'Access-Control-Allow-Headers',
